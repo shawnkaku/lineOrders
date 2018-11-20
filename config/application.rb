2b788e210e189f33436d265bdafce635d150156a
shawnkaku <shawnkuo67@gmail.com>
2018-11-20 14:30:46 +0800

    init

--- config/application.rb
+++ config/application.rb
@@ -15,11 +15,12 @@ module LineOrders
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'line.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
-    end
+    
+    # config.before_configuration do
+    #   env_file = File.join(Rails.root, 'config', 'line.yml')
+    #   YAML.load(File.open(env_file)).each do |key, value|
+    #     ENV[key.to_s] = value
+    #   end if File.exists?(env_file)
+    # end
   end
 end
