7f890fbe7c09f6d7ae05e98be65aab9fe0253a84
shawnkaku <shawnkuo67@gmail.com>
2018-11-21 15:47:01 +0800

    init

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -9,7 +9,7 @@ class LineController < ApplicationController
       }
   end
   def webhook
-    reply_text = simple_msg("5566!")
+    reply_text = simple_msg("OK!")
     # 傳送訊息
     response = reply_to_line(reply_text)
     # 回應 200
