011b1f58db0832dbf7e1434228918b4ebe692b7a
shawnkaku <shawnkuo67@gmail.com>
2018-12-07 16:55:17 +0800

    mark PENDING: Not yet implemented

--- spec/helpers/line_helper_spec.rb
+++ spec/helpers/line_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe LineHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe LineHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
