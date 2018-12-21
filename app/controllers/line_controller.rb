2ed7b3671e6300586b476526808f7225838e06de
shawnkaku <shawnkuo67@gmail.com>
2018-12-21 07:19:16 +0000

    create rich menu

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -9,8 +9,9 @@ class LineController < ApplicationController
       }
   end
   def webhook
-    create_rich_menu rich_menu
-    reply_text = simple_msg("RoR")
+    # create_rich_menu rich_menu
+    get_rich_menus 
+    reply_text = simple_msg("rich menu")
     # reply_text = rich_menu
     # 傳送訊息
     response = reply_to_line(reply_text)
